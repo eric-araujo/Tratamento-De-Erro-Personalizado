35f6a8be2decd8f6d4255b997b035d521ff3cfd7
Eric Araújo <eric.araujolima@hotmail.com>
2019-12-05 19:59:47 -0300

    Exercicio de fixação Aula 141 finalizado

---
+++ src/model/exception/AccountException.java
@@ -0,0 +1,9 @@
+package model.exception;
+
+public class AccountException extends Exception{
+	private static final long serialVersionUID = 1L;
+	
+	public AccountException(String msg) {
+		super(msg);
+	}
+}
